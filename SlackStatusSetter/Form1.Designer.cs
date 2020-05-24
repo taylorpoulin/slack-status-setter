1a4b244b4be8b37c91bdd74fe25bec2bae7a093e
Taylor <taylor.poulin@camis.com>
2020-05-24 16:50:18 -0400

    changing the icon

--- SlackStatusSetter/Form1.Designer.cs
+++ SlackStatusSetter/Form1.Designer.cs
@@ -93,6 +93,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(499, 354);
             this.Controls.Add(this.lbStatus);
             this.Controls.Add(this.label3);
